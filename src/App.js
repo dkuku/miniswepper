83a9e976054d21d8b72494a1dd2a876853ac2caa
Daniel Kukula <daniel.kuku@gmail.com>
2019-01-11 00:23:49 +0000

    basinc app working

--- src/App.js
+++ src/App.js
@@ -11,47 +11,86 @@ const Title = styled.p`
   width:100%;
 `
 class App extends Component {
-  state = { board: [[]],
-            width: 15,
-            height: 15 }
+  state = {
+    board: [[]],
+    width: 10,
+    height: 10,
+    emptyFields: 100
+  }
 
   componentDidMount() {
     this.setState({
-      board: this.genBoard(this.state.width, this.state.height, 0)
+      board: this.genBoard(this.state.width, this.state.height, 20)
     })
   }
 
-  revealField = (position) => {
-    this.setState(prevState => {
-      let newBoard = Array.from(prevState.board)
-      newBoard[position[0]][position[1]].open = this.countBombs(position)
-      return { board: newBoard }
-    })
-  }
-
-  countBombs(position) {
+  checkField = (position, board=this.state.board) => {
     const x = position[0]
     const y = position[1]
-    if (this.state.board[x][y].bomb) {
+    this.setState(prevState => {
+      return { emptyFields: prevState.emptyFields - 1 }
+    })
+    if (board[x][y].open) {
+      return board[x][y].open
+    }
+
+    const bombs = this.countBombs(position, board)
+    board[x][y].open = bombs
+    return board
+  }
+
+  revealField = (position, board=this.state.board) => {
+    const x = position[0]
+    const y = position[1]
+
+    board = this.checkField(position, board)
+    if (board[x][y].bomb){
+      this.revealBoard()
+    }
+    if (board[x][y].open === "0") {
+      this.openNeighbours(position, board)
+    }
+
+    this.setState(prevState => {
+      return { board, counter: prevState.counter+1 }
+    })
+  }
+
+  revealBoard(){
+    this.setState(prevState => {
+      return {board: prevState.board.map(row => row.map(field => {
+        field.open = this.countBombs(field.position, prevState.board)
+
+      }))}
+    })
+  }
+
+  openNeighbours(position, board) {
+    const x = position[0]
+    const y = position[1]
+    for (let i = x - 1; i <= x + 1; i++) {
+      for (let j = y - 1; j <= y + 1; j++) {
+        if (0 <= i && i < this.state.width && 0 <= j && j < this.state.height) {
+          if ( !board[i][j].open) {
+            this.revealField([i, j], board)
+          }
+        }
+      }
+    }
+  }
+
+  countBombs(position, board) {
+    const x = position[0]
+    const y = position[1]
+    if (board[x][y].bomb) {
       return true
     }
     let bombs = 0
     for (let i = x - 1; i <= x + 1; i++) {
       for (let j = y - 1; j <= y + 1; j++) {
         if (0 <= i && i < this.state.width && 0 <= j && j < this.state.height) {
-          if (this.state.board[i][j].bomb) {
+          if (board[i][j].bomb) {
             bombs++
-          }
-        }
-      }
-    }
-    if (!bombs) {
-      for (let i = x - 1; i <= x + 1; i++) {
-        for (let j = y - 1; j <= y + 1; j++) {
-          if (0 <= i && i < this.state.width && 0 <= j && j < this.state.height) {
-            if (!this.state.board[i][j].open){
-              this.revealField([i, j])
-            }
           }
         }
       }
@@ -60,9 +99,16 @@ class App extends Component {
   }
 
   genField(x, y, chance) {
+    const bomb = (Math.random() < chance / 100)
+    if (bomb) {
+      this.setState(prevState => {
+        return { emptyFields: prevState.emptyFields - 1 }
+      })
+    }
+
     return {
+      bomb,
       open: false,
-      bomb: (Math.random() < chance / 100),
       position: [x, y]
     }
   }
@@ -80,7 +126,7 @@ class App extends Component {
     const { board } = this.state
     return (
       <Board>
-        <Title>Saper</Title>
+        <Title>To go: {this.state.emptyFields} fields</Title>
         {board.map((row, i) =>
           <Row row={row} key={i} reveal={this.revealField}></Row>
         )}
