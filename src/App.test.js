341dfbfd35d6f10c866cc35a115e083b81a6b101
Daniel Kukula <daniel.kuku@gmail.com>
2019-01-11 00:20:40 +0000

    install enzyme, start with tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Enzyme, { shallow } from 'enzyme';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+Enzyme.configure({ adapter: new Adapter() });
+let wrapper
+describe("genField", () => {
+  beforeEach(()=>{
+    wrapper = shallow(<App />);
+  })
+  it('generates an empty field', () => {
+    expect(wrapper.instance().genField(0, 0, 0)).toEqual(
+      {
+        bomb: false,
+        open: false,
+        position: [0, 0]
+      }
+    )
+  })
+
+  it('generates a field wth bomb', () => {
+    expect(wrapper.instance().genField(1, 1, 100)).toEqual(
+      {
+        bomb: true,
+        open: false,
+        position: [1, 1]
+      }
+    )
+  })
+})
+
+describe("genBoard", () => {
+  beforeEach(()=>{
+    wrapper = shallow(<App />);
+  })
+  it('generates a board', () => {
+    expect(wrapper.instance().genBoard(1, 1, 0)).toEqual(
+      [[{
+        bomb: false,
+        open: false,
+        position: [0, 0]
+      }]]
+    )
+  })
+
+  it('generates a 10 x 10 board', () => {
+    const board = wrapper.instance().genBoard(10, 10, 0)
+    expect(board.length).toEqual(10)
+    expect(board[0].length).toEqual(10)
+  })
+})
+
+describe("countBombs", () => {
+  beforeEach(()=>{
+    wrapper = shallow(<App />);
+  })
+  it('finds 0 bombs on empty board', () => {
+    const board = wrapper.instance().genBoard(3, 3, 0)
+    expect(wrapper.instance().countBombs([1, 1], board)).toEqual("0")
+  })
+
+  it('finds bombs on full board', () => {
+    let board = wrapper.instance().genBoard(3, 3, 100)
+    board[0][0].bomb = false
+    board[1][1].bomb = false
+    expect(wrapper.instance().countBombs([0, 0], board)).toEqual("2")
+    expect(wrapper.instance().countBombs([1, 1], board)).toEqual("7")
+    expect(wrapper.instance().countBombs([2, 2], board)).toEqual(true)
+  })
+})
