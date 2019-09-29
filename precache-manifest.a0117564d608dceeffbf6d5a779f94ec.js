fc9ce6c419aa3c2aef76293692ce810f3831fba0
Daniel Kukula <daniel.kuku@gmail.com>
2019-09-29 20:56:54 +0100

    Updates

---
+++ precache-manifest.a0117564d608dceeffbf6d5a779f94ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a72b255a152734564026",
+    "url": "/miniswepper/static/js/runtime~main.a72b255a.js"
+  },
+  {
+    "revision": "fcd0aaf10bf6384377cc",
+    "url": "/miniswepper/static/js/main.fcd0aaf1.chunk.js"
+  },
+  {
+    "revision": "2c780649bda9eb7f4d5f",
+    "url": "/miniswepper/static/js/1.2c780649.chunk.js"
+  },
+  {
+    "revision": "fcd0aaf10bf6384377cc",
+    "url": "/miniswepper/static/css/main.5be5cc29.chunk.css"
+  },
+  {
+    "revision": "6b79dd5d04e027fa42b1561b5831cf58",
+    "url": "/miniswepper/index.html"
+  }
+];
